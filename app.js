0f825f202546c6647b741b0a2e2a30b9ef3cd507
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2022-12-09 15:58:02 +0100

    ♻️ update : parse data into the json

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-const createError= require('http-errors');
+const createError = require('http-errors');
 require('dotenv').config();
 require('./helper/init_mongodb');
 const AuthRoute = require('./Routes/Auth.route');
@@ -8,29 +8,30 @@ const AuthRoute = require('./Routes/Auth.route');
 const app = express();
 
 app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', async (req, res, next) => {
-    res.send("Hello from express"); 
+  res.send('Hello from express');
 });
 app.use('/auth', AuthRoute);
-app.use((req,res,next)=>{
-    const error = new Error('Not Found');
-    error.status = 404;
-    next(error);
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
 });
-app.use((err, req, res, next)=>{
-//    res.status(err.status || 500);
-//    res.send({
-//     error:{
-//     status: err.status || 500,
-//     message: err.message
-//     }
-//    });
-      next(createError.NotFound())
+app.use((err, req, res, next) => {
+  //   res.status(err.status || 500);
+  //   res.send({
+  //     error: {
+  //       status: err.status || 500,
+  //       message: err.message,
+  //     },
+  //   });
+  next(createError.NotFound());
 });
-
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT,()=>{
-   console.log(`Server is running port : ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running port : ${PORT}`);
 });
