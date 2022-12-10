99df81639f19647a1e64a0362a37ccc2474a78c6
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2022-12-10 15:59:43 +0100

    ♻️ Update : Add connection and DB name from env file

--- helper/init_mongodb.js
+++ helper/init_mongodb.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect(process.env.MONGODB_URI, { dbName: process.env.DB_NAME })
+  .connect(process.env.MONGODB_URI, {
+    dbName: process.env.DB_NAME,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log('Connection has been made 👍.');
   })
