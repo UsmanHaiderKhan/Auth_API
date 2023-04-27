916bb5290ef75663de24ba75ae3f762f3bd0130e
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2023-04-27 19:52:46 +0200

    ⚒️ Fix : email spell mistake

--- helper/validation_schema.js
+++ helper/validation_schema.js
@@ -1,7 +1,7 @@
 const Joi = require('@hapi/joi');
 
 const authSchema = Joi.object({
-  mail: Joi.string().email().lowercase().required(),
+  email: Joi.string().email().lowercase().required(),
   password: Joi.string().min(4).required(),
 });
 
