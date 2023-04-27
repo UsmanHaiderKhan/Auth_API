6625b4b840b3146cf729bfaa56d2b6de97032993
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2023-04-27 19:55:50 +0200

    ♻️ Update : add user validation schema

--- Models/User.model.js
+++ Models/User.model.js
@@ -1,10 +1,40 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
-    email: {type: String, required: true, lowercase: true, unique:true},
-    password: {type: String, required: true}  
+  email: { type: String, required: true, lowercase: true, unique: true },
+  password: { type: String, required: true },
 });
+
+UserSchema.pre('save', async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(8);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+UserSchema.methods.isValidPassword = async function (password) {
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (error) {
+    throw error;
+  }
+};
+
+// this is middle ware we use before POST
+
+// UserSchema.post('save', async function(next){
+//     try {
+//       console.log('Called after saving user');
+//     } catch (error) {
+
+//     }
+// });
 
 const User = mongoose.model('user', UserSchema);
 module.exports = User;
