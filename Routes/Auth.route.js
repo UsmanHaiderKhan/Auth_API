a23e2f91f5c423c0d1ec0a76a20d205c2f41f9b9
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2023-04-28 19:51:13 +0200

    ♻️ Update : did refactoring of the code

--- Routes/Auth.route.js
+++ Routes/Auth.route.js
@@ -1,43 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const createError = require('http-errors');
-const User = require('../Models/User.model');
-const { authSchema } = require('../helper/validation_schema');
+const AuthController = require('../Controllers/Auth.Controller');
 
-router.post('/register', async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-    const result = await authSchema.validateAsync(req.body);
-    console.log(result);
+router.post('/register', AuthController.register);
 
-    if (!email || !password) {
-      throw createError.BadRequest();
-    }
-    const doseExist = await User.findOne({ email: email });
-    if (doseExist) {
-      throw createError.Conflict(`${email} is already registered`);
-    }
-    const user = new User({ email, password });
-    const savedUser = await user.save();
-    res.send(savedUser);
-  } catch (error) {
-    if (error.isJoi) {
-      error.status = 422;
-    }
-    next(error);
-  }
-});
+router.post('/login', AuthController.login);
 
-router.post('/login', (req, res, next) => {
-  res.send('login Router');
-});
+router.post('/refresh-token', AuthController.refreshToken);
 
-router.post('/refresh-token', (req, res, next) => {
-  res.send('refresh-token Router');
-});
-
-router.delete('/logout', (req, res, next) => {
-  res.send('logout Router');
-});
+router.delete('/logout', AuthController.logout);
 
 module.exports = router;
